fade0aa5e25077ca311485676b2d03cd4ae1c039
Heta Björklund <heta.bjorklund@gofore.com>
2022-06-21 11:44:46 +0300

    Lista-komponentti luotu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
