ff4eb651410a201b633f5c8c9ee57cc517bc94e9
Heta Björklund <heta.bjorklund@gofore.com>
2022-06-27 15:29:05 +0300

    Routes siivottu pois AppModulesta
    Ovat jo AppRoutingModulessa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,35 +14,6 @@ import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoadingComponent } from './loading/loading.component';
 
-// const routes : Routes = [
-//   // tyhjä path on juuri eli etusivu, sillä halutaan näkyvän TabsComponent
-//   {
-//     path: 'characters',
-//     component: TabsComponent,
-//     children: [
-//     {
-//       path: '',
-//       redirectTo: 'all',
-//       pathMatch: 'full'
-//     },
-//     {
-//       path: ':side',
-//       component: ListComponent
-//     }
-//     ]
-//   },
-//   // /new-character on sivu jolla halutaan näkyvän CreateCharacterComponent
-//   {
-//     path: 'new-character',
-//     component: CreateCharacterComponent
-//   },
-//   // kaikki muut reitit uudelleenohjaavat juureen, joten jos käyttäjä kirjoittaa osoitteen jota ei ole olemassa, kaatumisen sijaan mennään etusivulle
-//   {
-//     path: '**',
-//     redirectTo: '/characters/all'
-//   }
-// ];
-
 @NgModule({
   declarations: [
     AppComponent,
